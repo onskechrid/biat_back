16d41ff8bd2e3e237e7bd004cc64ea04f4fa8d97
onskechrid <63127259+onskechrid@users.noreply.github.com>
2022-09-08 11:04:53 +0000

    fix

--- routes/index.js
+++ routes/index.js
@@ -121,7 +121,7 @@ router.post('/query/:table',  function(req, res, next) {
         res.send(jsoned);
       }
   });
-  });
+});
 router.get('/get-tables',  function(req, res, next) {
   var dbname;
   let tables;
@@ -143,6 +143,196 @@ router.get('/get-tables',  function(req, res, next) {
         });
         res.send(all)
     });
+});
+/* ******************************************************************************** */
+//ID
+router.get('/id2/:id_bct',  function(req, res, next) {
+  console.log("id_idbct");
+  connection.query(`select "ID" from public."E_CUSTOMER" where "ID_BCT"='${req.params.id_bct}';`, function(err, rows) {
+  console.log(`select "ID" from "E_CUSTOMER" where "ID_BCT"='${req.params.id_bct}';`);
+  let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].ID)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
+});
+router.get('/id3/:comp',  function(req, res, next) {
+  console.log("id_comp");
+  connection.query(`select "CUST" from "E_CONTRAT" where "COMP" = '${req.params.comp}' limit 1;`, function(err, rows) {
+  console.log(`select "CUST" from "E_CONTRAT" where "COMP" = '${req.params.comp}' limit 1;`);
+  let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].CUST)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
+});
+router.get('/id4/:refcrdt',  function(req, res, next) {
+  console.log("id_refcrdt");
+  connection.query(`select "CUST" from "E_CONTRAT" where "REF_CRDT" = '${req.params.refcrdt}';`, function(err, rows) {
+  console.log(`select "CUST" from "E_CONTRAT" where "REF_CRDT" = '${req.params.refcrdt}';`);
+  let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].CUST)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
+});
+// NAME
+router.get('/name1/:id_customer',  function(req, res, next) {
+  console.log("name_idcustomer");
+  connection.query(`select "NAME_1" from public."E_CUSTOMER" where "ID"='${req.params.id_customer}';`, function(err, rows) {
+    console.log(`select "NAME_1" from "E_CUSTOMER" where "ID"='${req.params.id_customer}';`);
+    let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].NAME_1)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
+});
+router.get('/name2/:id_bct',  function(req, res, next) {
+  console.log("name_idbct");
+  connection.query(`select "NAME_1" from public."E_CUSTOMER" where "ID_BCT"='${req.params.id_bct}';`, function(err, rows) {
+    console.log(`select "NAME_1" from "E_CUSTOMER" where "ID_BCT"='${req.params.id_bct}';`);
+    let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].NAME_1)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
+});
+router.get('/name3/:comp',  function(req, res, next) {
+  console.log("name_comp");
+  connection.query(`select ec."NAME_1" from "E_CUSTOMER" ec inner join "E_CONTRAT" econ on ec."ID" = econ."CUST" where econ."COMP"='${req.params.comp}' limit 1;`, function(err, rows) {
+    console.log(`select ec."NAME_1" from "E_CUSTOMER" ec inner join "E_CONTRAT" econ on ec."ID" = econ."CUST" where econ."COMP"='${req.params.comp}' limit 1;`);
+    let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].NAME_1)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
+});
+router.get('/name4/:refcrdt',  function(req, res, next) {
+  console.log("name_refcrdt");
+  connection.query(`select "NAME_1" from "E_CUSTOMER" ec inner join "E_CONTRAT" econ on ec."ID" = econ."CUST" where econ."REF_CRDT" ='${req.params.refcrdt}' limit 1;`, function(err, rows) {
+    console.log(`select "NAME_1" from "E_CUSTOMER" ec inner join "E_CONTRAT" econ on ec."ID" = econ."CUST" where econ."REF_CRDT" ='${req.params.refcrdt}' limit 1;`);
+    let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].NAME_1)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
+});
+//ID_BCT
+router.get('/idbct1/:id_customer',  function(req, res, next) {
+    console.log("idbct_id_customer");
+    connection.query(`select "ID_BCT" from "E_CUSTOMER" ec where "ID" ='${req.params.id_customer}';`, function(err, rows) {
+      console.log(`select "ID_BCT" from "E_CUSTOMER" ec where "ID" ='${req.params.id_customer}';`);
+      console.log(rows.rows[0])
+      res.send(rows.rows[0])
+  });
+});
+router.get('/idbct3/:comp',  function(req, res, next) {
+  console.log("idbct_comp");
+  connection.query(`select ec."ID_BCT" from "E_CUSTOMER" ec inner join "E_CONTRAT" econ on ec."ID" = econ."CUST" where econ."COMP"='${req.params.comp}' limit 1;`, function(err, rows) {
+    console.log(`select ec."ID_BCT" from "E_CUSTOMER" ec inner join "E_CONTRAT" econ on ec."ID" = econ."CUST" where econ."COMP"='${req.params.comp}' limit 1;`);
+    console.log(rows.rows[0])
+    res.send(rows.rows[0])
+});
+});
+router.get('/idbct4/:refcrdt',  function(req, res, next) {
+  console.log("idbct_refcrdt");
+  connection.query(`select "ID_BCT" from "E_CUSTOMER" ec inner join "E_CONTRAT" econ on ec."ID" = econ."CUST" where econ."REF_CRDT" ='${req.params.refcrdt}' limit 1;`, function(err, rows) {
+    console.log(`select "ID_BCT" from "E_CUSTOMER" ec inner join "E_CONTRAT" econ on ec."ID" = econ."CUST" where econ."REF_CRDT" ='${req.params.refcrdt}' limit 1;`);
+    console.log(rows.rows[0])
+    res.send(rows.rows[0])
+});
+});
+// LIBL
+router.get('/lib1/:id_customer',  function(req, res, next) {
+  console.log("lib_idcostumer");
+  connection.query(`select ra."LIBL" from "R_ACTIVITE" ra inner join "E_CUSTOMER" ec on ra."ID" = ec."CU_ACTV" where ec."ID" ='${req.params.id_customer}';`, function(err, rows) {
+    console.log(`select ra."LIBL" from "R_ACTIVITE" ra inner join "E_CUSTOMER" ec on ra."ID" = ec."CU_ACTV" where ec."ID" ='${req.params.id_customer}';`);
+    let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].LIBL)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
+});
+router.get('/lib2/:id_bct',  function(req, res, next) {
+  console.log("lib_idbct");
+  connection.query(`select ra."LIBL" from "R_ACTIVITE" ra inner join "E_CUSTOMER" ec on ra."ID" = ec."CU_ACTV" where ec."ID_BCT" ='${req.params.id_bct}';`, function(err, rows) {
+    console.log(`select ra."LIBL" from "R_ACTIVITE" ra inner join "E_CUSTOMER" ec on ra."ID" = ec."CU_ACTV" where ec."ID_BCT" ='${req.params.id_bct}';`);
+    let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].LIBL)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
+});
+router.get('/lib3/:comp',  function(req, res, next) {
+  console.log("lib_comp");
+  connection.query(`select ra."LIBL" from "R_ACTIVITE" ra inner join "E_CUSTOMER" ec on ra."ID" = ec."CU_ACTV" where ec."ID" = (select "CUST" from "E_CONTRAT" econ where econ."COMP" ='${req.params.comp}' limit 1);`, function(err, rows) {
+    console.log(`select ra."LIBL" from "R_ACTIVITE" ra inner join "E_CUSTOMER" ec on ra."ID" = ec."CU_ACTV" where ec."ID" = (select "CUST" from "E_CONTRAT" econ where econ."COMP" ='${req.params.comp}' limit 1);`);
+    let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].LIBL)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
+});
+router.get('/lib4/:refcrdt',  function(req, res, next) {
+  console.log("lib_refcrdt");
+  connection.query(`select ra."LIBL" from "R_ACTIVITE" ra inner join "E_CUSTOMER" ec on ra."ID" = ec."CU_ACTV" where ec."ID" = (select "CUST" from "E_CONTRAT" econ where econ."REF_CRDT" ='${req.params.refcrdt}');`, function(err, rows) {
+    console.log(`select ra."LIBL" from "R_ACTIVITE" ra inner join "E_CUSTOMER" ec on ra."ID" = ec."CU_ACTV" where ec."ID" = (select "CUST" from "E_CONTRAT" econ where econ."REF_CRDT" ='${req.params.refcrdt}');`);
+    let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].LIBL)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
+});
+/********************************************************************************** */
+
+router.post('/query/:clicked_filtre/:value/:datasit',  function(req, res, next) {
+  connection.query(`${req.body.query} where '${req.params.clicked_filtre}'='${req.params.value}' AND "DAT_SIT"='${req.params.datasit}';`, function(err, rows) {
+      if(err){
+        res.send(null)
+        console.log("rrrr")
+      }else{
+        //console.log(rows.rows);
+        //console.log(tables);
+        var jsoned = Object.values(JSON.parse(JSON.stringify(rows.rows)));
+        //console.log(jsoned);
+        res.send(jsoned);
+        console.log("rrrr222")
+      }
+  });
+});
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+router.get('/alldatsit', function(req, res, next){
+  connection.query(`select "DAT_SIT" from public."E_ENCOURS" order by "DAT_SIT" DESC;`, function(err, rows){
+    if(err) throw err
+    let arr = []
+    for(let i=0; i<= rows.rows.length-1; i++){
+        arr.push(rows.rows[i].DAT_SIT)
+    }
+    console.log(arr)
+    res.send(arr)
+  });
 });
 
 router.get('/show-function',  function(req, res, next) {
@@ -227,3 +417,5 @@ router.delete('/delete-function/:id',  function(req, res, next) {
     });
 });
 module.exports = router;
+
+
